b1ed6a4d44b232ff796ee97bf1cb9699f6da91a8
Andhitia Rama <andhitia.r@gmail.com>
2017-06-11 03:14:00 +0700

    [l10n_id_taxform_bukti_potong_pph_f113302

---
+++ l10n_id_taxform_bukti_potong_pph_f113302/__openerp__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Indonesia - Bukti Potong PPh 21  Final (F.1.1.33.02)",
+    "version": "8.0.1.0.0",
+    "category": "localization",
+    "website": "https://opensynergy-indonesia.com/",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "l10n_id_taxform_bukti_potong_pph_common",
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "data/l10n_id_bukti_potong_type.xml",
+        "views/bukti_potong_pph_f113302_out_views.xml",
+    ],
+}
---
+++ l10n_id_taxform_bukti_potong_pph_f113302/models/bukti_potong_pph_f113302_out.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, fields, api
+
+
+class BuktiPotongPPhF113302Out(models.Model):
+    _name = "l10n_id.bukti_potong_pph_f113302_out"
+    _inherit = "l10n_id.bukti_potong_pph"
+    _table = "l10n_id_bukti_potong_pph"
+    _description = "Bukti Potong PPh f.1.1.33.02 Out"
+
+    @api.model
+    def _default_type_id(self):
+        return self.env.ref(
+            "l10n_id_taxform_bukti_potong_pph_f113302."
+            "bukti_potong_pph_type_f113302_out").id
+
+    type_id = fields.Many2one(
+        default=lambda self: self._default_type_id(),
+    )
+
+    @api.model
+    def search(self, args, offset=0, limit=None, order=None):
+        type_id = self.env.ref(
+            "l10n_id_taxform_bukti_potong_pph_f113302."
+            "bukti_potong_pph_type_f113302_out")
+        args.append(("type_id", "=", type_id.id))
+        return super(BuktiPotongPPhF113302Out, self).\
+            search(args, offset, limit, order)

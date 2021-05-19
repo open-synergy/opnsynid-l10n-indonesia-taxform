2bb0c317545d971bcd4336894cab2818af34fa88
azmimr67@gmail.com <azmimr67@gmail.com>
2021-05-19 16:57:11 +0700

    [l10n_id_taxform_1721a1] 8.0.1.2.0
    Fix #6, #8, #9

--- l10n_id_taxform_1721a1/__openerp__.py
+++ l10n_id_taxform_1721a1/__openerp__.py
@@ -5,7 +5,7 @@
 # pylint: disable=locally-disabled, manifest-required-author
 {
     "name": "Indonesia - Form Bukti Potong 1721 A1",
-    "version": "8.0.1.1.0",
+    "version": "8.0.1.2.0",
     "category": "localization",
     "website": "https://simetri-sinergi.id",
     "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",

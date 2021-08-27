4e9dc0a31eb0077251ce460bec782d90c6e8226f
Nurazmi <azmimr67@gmail.com>
2021-08-27 19:38:27 +0700

    l10n_id_taxform_bukti_potong_pph_f113302 Task T003692

--- l10n_id_taxform_bukti_potong_pph_f113302/__openerp__.py
+++ l10n_id_taxform_bukti_potong_pph_f113302/__openerp__.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 # Copyright 2017 OpenSynergy Indonesia
+# Copyright 2021 PT. Simetri Sinergi Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Indonesia - Bukti Potong PPh 21  Final (F.1.1.33.02)",
-    "version": "8.0.1.1.0",
+    "version": "8.0.1.2.0",
     "category": "localization",
-    "website": "https://opensynergy-indonesia.com/",
-    "author": "OpenSynergy Indonesia",
+    "website": "https://simetri-sinergi.id",
+    "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
     "license": "AGPL-3",
     "application": False,
     "installable": True,
@@ -16,6 +17,10 @@
     "data": [
         "security/ir.model.access.csv",
         "data/l10n_id_bukti_potong_type.xml",
+        "data/ir_sequence_data.xml",
+        "data/base_sequence_configurator_data.xml",
+        "data/base_workflow_policy_pph_f113302_out_data.xml",
+        "data/base_cancel_reason_configurator_data.xml",
         "views/bukti_potong_pph_f113302_out_views.xml",
     ],
 }
--- l10n_id_taxform_bukti_potong_pph_f113302/models/__init__.py
+++ l10n_id_taxform_bukti_potong_pph_f113302/models/__init__.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
 # Copyright 2017 OpenSynergy Indonesia
+# Copyright 2021 PT. Simetri Sinergi Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from . import bukti_potong_pph_f113302_out
+from . import (
+    bukti_potong_pph_f113302_out,
+    tier_definition,
+)

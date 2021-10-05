e17020f18901ec54538bb019d17cc1aaeb8513ba
mikevhe18 <viriyananda.michael@gmail.com>
2021-10-05 10:42:49 +0700

    Revisi

--- l10n_id_taxform_bukti_potong_pph_23/models/bukti_potong_pph_23_in.py
+++ l10n_id_taxform_bukti_potong_pph_23/models/bukti_potong_pph_23_in.py
@@ -2,7 +2,8 @@
 # Copyright 2017 OpenSynergy Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import api, fields, models
+from openerp import _, api, fields, models
+from openerp.exceptions import Warning as UserError
 
 
 class BuktiPotongPPh23In(models.Model):
@@ -11,11 +12,20 @@ class BuktiPotongPPh23In(models.Model):
     _table = "l10n_id_bukti_potong_pph"
     _description = "Bukti Potong PPh 23 In"
 
+    @api.multi
+    def _get_type_id(self):
+        self.ensure_one()
+        type_id = self.env.ref(
+            "l10n_id_taxform_bukti_potong_pph_23." "bukti_potong_pph_type_f113306_in"
+        )
+        if not type_id.id:
+            err_msg = _("Bukti Potong PPh 23 In type hasn't defined")
+            raise UserError(err_msg)
+        return type_id.id
+
     @api.model
     def _default_type_id(self):
-        return self.env.ref(
-            "l10n_id_taxform_bukti_potong_pph_23." "bukti_potong_pph_type_f113306_in"
-        ).id
+        return self._get_type_id()
 
     type_id = fields.Many2one(
         default=lambda self: self._default_type_id(),
@@ -23,10 +33,8 @@ class BuktiPotongPPh23In(models.Model):
 
     @api.model
     def search(self, args, offset=0, limit=None, order=None, count=False):
-        type_id = self.env.ref(
-            "l10n_id_taxform_bukti_potong_pph_23." "bukti_potong_pph_type_f113306_in"
-        )
-        args.append(("type_id", "=", type_id.id))
+        type_id = self._get_type_id()
+        args.append(("type_id", "=", type_id))
         return super(BuktiPotongPPh23In, self).search(
             args=args, offset=offset, limit=limit, order=order, count=count
         )

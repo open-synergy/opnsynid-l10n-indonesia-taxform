83ef196ac9a6d551c76e1ac6d464bc9524a8f6df
Nurazmi <azmimr67@gmail.com>
2022-06-29 11:34:30 +0700

    [ssi_l10n_id_taxform_bukti_potong_pph_f113306] 14.0.1.0.0

---
+++ ssi_l10n_id_taxform_bukti_potong_pph_f113306/__manifest__.py
@@ -0,0 +1,30 @@
+# Copyright 2022 OpenSynergy Indonesia
+# Copyright 2022 PT. Simetri Sinergi Indonesia
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+# pylint: disable=locally-disabled, manifest-required-author
+{
+    "name": "Indonesia - Bukti Potong PPh 23 (f.1.33.06)",
+    "version": "14.0.1.0.0",
+    "category": "localization",
+    "website": "https://simetri-sinergi.id",
+    "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
+    "license": "LGPL-3",
+    "installable": True,
+    "application": True,
+    "depends": [
+        "ssi_l10n_id_taxform_bukti_potong_pph_mixin",
+    ],
+    "data": [
+        "security/ir_module_category_data.xml",
+        "security/res_group_data.xml",
+        "security/ir.model.access.csv",
+        "security/ir_rule_data.xml",
+        "data/l10n_id_bukti_potong_type.xml",
+        "data/ir_sequence_data.xml",
+        "data/sequence_template_data.xml",
+        "data/policy_template_data.xml",
+        "data/approval_template_data.xml",
+        "views/bukti_potong_pph_f113306_in_views.xml",
+        # "views/bukti_potong_pph_views.xml",
+    ],
+}

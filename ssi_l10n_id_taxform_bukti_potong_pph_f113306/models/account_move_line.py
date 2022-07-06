c3f6802efdfc76ff6a7c4e2e40099bf6e0c19516
Nurazmi <azmimr67@gmail.com>
2022-07-06 19:05:00 +0700

    [ssi_l10n_id_taxform_bukti_potong_pph_f113306] 14.0.1.2.0
    * Add bukpot out

--- ssi_l10n_id_taxform_bukti_potong_pph_f113306/models/account_move_line.py
+++ ssi_l10n_id_taxform_bukti_potong_pph_f113306/models/account_move_line.py
@@ -9,7 +9,7 @@ from odoo.exceptions import Warning as UserError
 class AccountMoveLine(models.Model):
     _inherit = "account.move.line"
 
-    def _check_taxform(self):
+    def _check_taxform_in(self):
         self.ensure_one()
         self._cr.execute(
             """SELECT 1 FROM rel_bukpot_f113306_in_line_2_income_move
@@ -22,10 +22,27 @@ class AccountMoveLine(models.Model):
         else:
             return False
 
+    def _check_taxform_out(self):
+        self.ensure_one()
+        self._cr.execute(
+            """SELECT 1 FROM rel_bukpot_f113306_out_line_2_income_move
+            WHERE account_move_id=%s
+            """,
+            (self.id,),
+        )
+        if self._cr.fetchone():
+            return True
+        else:
+            return False
+
     def unlink(self):
         for move_line in self:
-            taxform = move_line._check_taxform()
-            if taxform:
+            taxform_in = move_line._check_taxform_in()
+            if taxform_in:
+                strWarning = _("You cannot delete journal item linked to taxform!")
+                raise UserError(strWarning)
+            taxform_out = move_line._check_taxform_out()
+            if taxform_out:
                 strWarning = _("You cannot delete journal item linked to taxform!")
                 raise UserError(strWarning)
         return super(AccountMoveLine, self).unlink()

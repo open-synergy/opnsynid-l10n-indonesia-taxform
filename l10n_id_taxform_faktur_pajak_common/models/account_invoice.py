e3fe6df47aa8d1aa97773de3a48a6afb78e1bf87
Andhitia Rama <andhitia.r@gmail.com>
2022-11-25 14:11:49 +0700

    [l10n_id_taxform_faktur_pajak_common] 8.0.1.0.2

--- l10n_id_taxform_faktur_pajak_common/models/account_invoice.py
+++ l10n_id_taxform_faktur_pajak_common/models/account_invoice.py
@@ -26,7 +26,13 @@ class AccountInvoice(models.Model):
         for record in self:
             result = []
             if record.partner_id:
-                criteria = [("commercial_partner_id", "=", record.partner_id.id)]
+                criteria = [
+                    (
+                        "commercial_partner_id",
+                        "=",
+                        record.commercial_partner_id.partner_id.id,
+                    )
+                ]
                 result = ResParter.search(criteria).ids
             record.allowed_taxform_address_ids = result
 

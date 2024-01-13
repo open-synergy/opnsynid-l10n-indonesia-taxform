beb04c35ccde4a5783cd8a4478cbf8449445de78
Andhitia Rama <andhitia.r@gmail.com>
2024-01-13 16:02:20 +0700

    [ADD] ssi_l10n_id_taxform_faktur_pajak

--- ssi_l10n_id_taxform_faktur_pajak/__manifest__.py
+++ ssi_l10n_id_taxform_faktur_pajak/__manifest__.py
@@ -29,6 +29,7 @@
         "security/ir_module_category_data.xml",
         "security/res_group_data.xml",
         "security/ir.model.access.csv",
+        "security/ir_rule_data.xml",
         "data/ir_sequence_data.xml",
         "data/sequence_template_data.xml",
         "data/approval_template_data.xml",

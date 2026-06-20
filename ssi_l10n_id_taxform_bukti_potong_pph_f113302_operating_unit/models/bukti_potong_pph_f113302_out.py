bc603bd93b5c3886c34671ad250e8c3718d4fc8a
Andhitia Rama <andhitia.r@gmail.com>
2026-06-20 07:50:27 +0700

    [ADD] ssi_l10n_id_taxform_bukti_potong_pph_f113302_operating_unit
    Integrasi Operating Unit untuk Bukti Potong PPh 21 Final (f.1.1.33.02). Menambahkan operating_unit_id pada dokumen keluaran (_out) beserta record rule dan view.

---
+++ ssi_l10n_id_taxform_bukti_potong_pph_f113302_operating_unit/models/bukti_potong_pph_f113302_out.py
@@ -0,0 +1,13 @@
+# Copyright 2026 OpenSynergy Indonesia
+# Copyright 2026 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import models
+
+
+class BuktiPotongPPhf113302Out(models.Model):
+    _name = "l10n_id.bukti_potong_pph_f113302_out"
+    _inherit = [
+        "l10n_id.bukti_potong_pph_f113302_out",
+        "mixin.single_operating_unit",
+    ]
